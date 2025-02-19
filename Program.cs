2535a76ff3c8953a87c28ff5bf56276f9e83845e
Nygaaard <nygardandreas@hotmail.com>
2025-02-19 09:25:37 +0100

    Configured SQLite database connection in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using library.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Activate MVC
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
